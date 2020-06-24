ed8301b6fb8f4496bbe9269240158c8d759fb232
caleb <caleb.davidson@cqumail.com>
2020-06-24 14:48:27 +1000

    Added getters and setters and switched counting system to use qelapsed timer

--- IrTripWire/irtripwire.cpp
+++ IrTripWire/irtripwire.cpp
@@ -1,5 +1,112 @@
 #include "irtripwire.h"
+#include "WiringPi-master/wiringPi/wiringPi.h"
+//#include <wiringPi.h>
+//#include <unistd.h>
+#include <iostream>
+#include <qobject.h>
+#include <qdebug.h>
+#include <QElapsedTimer>
 
-IrTripWire::IrTripWire()
+bool stopLoop = false;
+void sensorLoop();
+unsigned long *loopDelay;
+unsigned long *triggerDelay;
+int *triggerValue;
+
+IrTripWire::IrTripWire(unsigned long loopDelayMicroSecs, unsigned long triggerDelayMicroSecs, int triggerValueUnit)
 {
+    //The delay when checking if the capacitor charge time is above specified value
+    loopDelay = new unsigned long(loopDelayMicroSecs);
+    triggerDelay = new unsigned long(triggerDelayMicroSecs);
+    triggerValue = new int(triggerValueUnit);
+
+}
+
+void IrTripWire::run()
+{
+    sensorLoop();
+}
+void IrTripWire::exit()
+{
+    stopLoop = true;
+
+}
+
+bool IrTripWire::setLoopDelay(unsigned long loopDelayMicroSecs)
+{
+    *loopDelay = loopDelayMicroSecs;
+    return true;
+}
+
+bool IrTripWire::setTriggerDelay(unsigned long triggerDelayMicroSecs)
+{
+    *triggerDelay = triggerDelayMicroSecs;
+    return true;
+}
+
+bool IrTripWire::setTriggerValueDelay(unsigned long triggerValueUnit)
+{
+    *triggerValue = triggerValueUnit;
+    return true;
+}
+
+unsigned long *IrTripWire::getLoopDelay()
+{
+return loopDelay;
+}
+
+unsigned long * IrTripWire::getTriggerDelay()
+{
+return triggerDelay;
+}
+
+int * IrTripWire::getTriggerValueDelay()
+{
+return triggerValue;
+}
+
+
+
+
+void IrTripWire::sensorLoop()
+{
+    QElapsedTimer  *timer = new QElapsedTimer();
+
+
+    qInfo() << "Sensor loop started";
+    while(stopLoop == false)
+    {
+        timer->restart();
+        timer->start();
+        //int count = 0;
+        pinMode (0, OUTPUT) ;
+        digitalWrite (0,  LOW) ;
+
+        usleep(*loopDelay);
+        pinMode (0, INPUT) ;
+
+
+        while(digitalRead(0) == LOW && stopLoop == false)
+        {
+            //count += 1;
+
+        }
+
+
+        //std::cout << count << std::endl;
+
+
+        if(timer->elapsed() > 1000)
+        {
+            std::cout << "sending signal" << std::endl;
+            emit triggered(timer->elapsed());
+            usleep(*triggerDelay);
+//            digitalWrite(7, HIGH);
+//            usleep(1000000);
+//            digitalWrite(7, LOW);
+
+        }
+    }
+    std::cout << "Sensor stopping" << std::endl;
+
 }
--- IrTripWire/irtripwire.h
+++ IrTripWire/irtripwire.h
@@ -2,3 +2,27 @@
 #define IRTRIPWIRE_H
 
 #endif // IRTRIPWIRE_H
+#include <QObject>
+#include <QThread>
+
+class IrTripWire : public QThread
+{
+    Q_OBJECT
+private:
+
+public:
+    IrTripWire(unsigned long loopDelayMicroSecs, unsigned long triggerDelayMicroSecs, int triggerValueUnit);
+    void sensorLoop();
+    void run();
+    void exit();
+    bool setLoopDelay(unsigned long loopDelayMicroSecs);
+    bool setTriggerDelay(unsigned long triggerDelayMicroSecs);
+    bool setTriggerValueDelay(unsigned long triggerValueUnit);
+    unsigned long *getLoopDelay();
+    unsigned long *getTriggerDelay();
+    int *getTriggerValueDelay();
+
+
+signals:
+void triggered(qint64 newValue);
+};
